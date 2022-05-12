c9455d7b19f76f3c2a72594236a1ae687deceab1
BogdanZavadka <bogdanzavadka@gmail.com>
2022-05-12 21:21:35 +0300

    fixed import*

--- lab21/src/test/java/iot/PetShop/manager/RedactorTest.java
+++ lab21/src/test/java/iot/PetShop/manager/RedactorTest.java
@@ -3,8 +3,6 @@ package iot.PetShop.manager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class RedactorTest {
 
     @Test
